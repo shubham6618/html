ea7c579e6769e0a85f411a5667c27afe71c17383
Shubham Sharma <Shubham Sharma>
2016-09-19 10:28:19 +0530

    9125

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                    ×</button>
+                    &times</button>
                 <h4 class="modal-title" id="myModalLabel">
                     Login/Registration</h4>
             </div>

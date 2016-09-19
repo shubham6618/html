77dd523c04e27ed33de017add5ff564b2aec61aa
Shubham Sharma <Shubham Sharma>
2016-09-19 10:25:07 +0530

    8498

--- index.php
+++ index.php
@@ -10,6 +10,40 @@
 
 	<!-- Latest compiled JavaScript -->
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+	<style type="text/css">
+		.nav-tabs {
+	    margin-bottom: 15px;
+		}
+		.sign-with {
+	    margin-top: 25px;
+	    padding: 20px;
+		}
+		div#OR {
+	    height: 30px;
+	    width: 30px;
+	    border: 1px solid #C2C2C2;
+	    border-radius: 50%;
+	    font-weight: bold;
+	    line-height: 28px;
+	    text-align: center;
+	    font-size: 12px;
+	    float: right;
+	    position: absolute;
+	    right: -16px;
+	    top: 40%;
+	    z-index: 1;
+	    background: #DFDFDF;
+		}
+
+
+
+	</style>
+	<script type="text/javascript">
+		$('#myModal').modal('show');
+		
+	</script>
 </head>
 <body>
 	<!-- Large modal -->

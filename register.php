3750fdb3a40489fbc14a5f2238ef83885911fe6d
Shubham Sharma <Shubham Sharma>
2016-09-19 12:39:23 +0530

    2038

--- register.php
+++ register.php
@@ -1,8 +1,30 @@
 <?php
 
 	include 'connection.php';
+	if (isset($_POST['register'])) {
+		
+		$title=mysql_real_escape_string(trim($_POST['title']));
+		$first_name=mysql_real_escape_string(trim($_POST['firstName']));
+		$last_name=mysql_real_escape_string(trim($_POST['lastName']));
+		$email=mysql_real_escape_string(trim($_POST['email']));
+		$mobile=mysql_real_escape_string(trim($_POST['mobile']));
+		$password1=mysql_real_escape_string(trim($_POST['password1']));
+		$password2=mysql_real_escape_string(trim($_POST['password2']));
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+   			 
+		}
+		else
+			
+			header("Location: index.php?msg=email");
+		if ($password1 === $password2) {
+			
+		} else {
+			
+			header("Location: index.php?msg=password");
+		}
+		
+	}
 
-	echo $_POST;
 
 
 ?>

99d09428ece4a73287fa9d838bc6c8fb8ba83ef5
Shubham Sharma <Shubham Sharma>
2016-09-19 11:17:54 +0530

    18840

--- index.php
+++ index.php
@@ -148,8 +148,8 @@
                                     <div class="col-sm-2">
                                     </div>
                                     <div class="col-sm-10">
-                                        <button type="button" name="register" class="btn btn-primary btn-sm">
-                                            Save & Continue</button>
+                                        <input type="submit" name="register" class="btn btn-primary btn-sm">
+                                            Sign up</input>
                                         <button type="button" class="btn btn-default btn-sm">
                                             Cancel</button>
                                     </div>
